af5f6f0654a92117ce26efc3b13c7dcfdf955006
Diaan Engelbrecht <diaan@pop-os.localdomain>
2021-09-11 10:58:39 +0200

    Added basic movement and broken rotations.

--- Spaceship.gd
+++ Spaceship.gd
@@ -1,11 +1,31 @@
 extends KinematicBody2D
 
+# Global constants
+const MOVEMENT_SPEED = 10
 
-# Called when the node enters the scene tree for the first time.
+# Global variables
+var motion
+
 func _ready():
-	pass # Replace with function body.
+	motion = Vector2.ZERO
+
+func _physics_process(delta):
+	var direction = get_movement_direction()
+	motion += direction * MOVEMENT_SPEED
+	move_and_slide(motion)
+	rotate(position.angle_to(get_global_mouse_position()))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func get_movement_direction():
+	var direction = Vector2.ZERO
+
+	if Input.is_action_pressed("move_down"):
+		direction += Vector2(0,1)
+	if Input.is_action_pressed("move_up"):
+		direction += Vector2(0,-1)
+	if Input.is_action_pressed("move_right"):
+		direction += Vector2(1,0)
+	if Input.is_action_pressed("move_left"):
+		direction += Vector2(-1,0)
+
+	return direction
